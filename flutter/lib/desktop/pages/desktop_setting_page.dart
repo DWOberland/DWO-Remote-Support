4c3ce370a0dc59006588df0b36f692246e31c3d6
DWOberland <paetzoldt@dwoberland.de>
2026-06-04 20:49:26 +0200

    Update desktop_setting_page.dart

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2384,7 +2384,7 @@ class _AboutState extends State<_About> {
       final scrollController = ScrollController();
       return SingleChildScrollView(
         controller: scrollController,
-        child: _Card(title: translate('About RustDesk'), children: [
+        child: _Card(title: translate('About DWO Client'), children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -2403,7 +2403,7 @@ class _AboutState extends State<_About> {
                         .marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com/privacy.html');
+                    launchUrlString('https://www.digitalwerk-oberland.de/datenschutz');
                   },
                   child: Text(
                     translate('Privacy Statement'),
@@ -2411,7 +2411,7 @@ class _AboutState extends State<_About> {
                   ).marginSymmetric(vertical: 4.0)),
               InkWell(
                   onTap: () {
-                    launchUrlString('https://rustdesk.com');
+                    launchUrlString('https://www.digitalwerk-oberland.de');
                   },
                   child: Text(
                     translate('Website'),
@@ -2429,7 +2429,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Digitalwerk Oberland\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
